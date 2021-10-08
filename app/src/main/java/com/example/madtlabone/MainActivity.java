38c557a3f0866f9e51c9e98cbda41ccfca44bc13
Faisal-Kazeem <faisalkazeem50@gamil.com>
2021-10-08 11:53:39 +0000

    Second commit: added functionality to change TextView

--- app/src/main/java/com/example/madtlabone/MainActivity.java
+++ app/src/main/java/com/example/madtlabone/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.madtlabone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtVwMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.txtVwMain = findViewById(R.id.txtVwMain);
+    }
+
+    public void onBtnClick(View view) {
+        this.txtVwMain.setText("The text was changed.");
     }
 }
