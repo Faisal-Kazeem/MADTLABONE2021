53c8c87cb3136697ab17fc14a8a5048b4d28528c
Faisal-Kazeem <faisalkazeem50@gamil.com>
2021-10-10 09:40:43 +0000

    Third commit: added functionality to change TextView text colour.

--- app/src/main/java/com/example/madtlabone/MainActivity.java
+++ app/src/main/java/com/example/madtlabone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.madtlabone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -19,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick(View view) {
         this.txtVwMain.setText("The text was changed.");
+    }
+
+    @SuppressLint("ResourceAsColor")
+    public void onBtnClick2(View view) {
+        this.txtVwMain.setTextColor(R.color.purple_200);
     }
 }
